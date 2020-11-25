0cecb93327d04096d9f4930d560462eb081edc9a
pathan-din <pathand@triazinesoft.com>
2020-11-25 18:53:09 +0530

    Add new components

---
+++ src/app/modules/ei/authorisation/authorisations-request/authorisations-request.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-authorisations-request',
+  templateUrl: './authorisations-request.component.html',
+  styleUrls: ['./authorisations-request.component.css']
+})
+export class AuthorisationsRequestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/modules/ei/authorisation/view-ei-profile-and-timeline/view-ei-profile-and-timeline.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-ei-profile-and-timeline',
+  templateUrl: './view-ei-profile-and-timeline.component.html',
+  styleUrls: ['./view-ei-profile-and-timeline.component.css']
+})
+export class ViewEiProfileAndTimelineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
