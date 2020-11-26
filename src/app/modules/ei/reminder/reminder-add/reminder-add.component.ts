9495427f5a3be37c36e77dbab228f1b030fc7245
pathan-din <pathand@triazinesoft.com>
2020-11-26 18:15:59 +0530

    Add components add input

--- src/app/modules/ei/reminder/reminder-add/reminder-add.component.ts
+++ src/app/modules/ei/reminder/reminder-add/reminder-add.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {ThemePalette} from '@angular/material/core';
+
+export interface Task {
+  name: string;
+  completed: boolean;
+  color: ThemePalette;
+  subtasks?: Task[];
+}
 
 @Component({
   selector: 'app-reminder-add',
@@ -10,6 +18,39 @@ export class ReminderAddComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  task: Task = {
+    name: 'M.Com',
+    completed: false,
+    color: 'primary',
+    subtasks: [
+      {name: '1st Year', completed: false, color: 'primary'},
+      {name: '2nd Year', completed: false, color: 'primary'},
+      {name: '3rd Year', completed: false, color: 'primary'},
+      {name: '4th Year', completed: false, color: 'primary'}
+    ]
+  };
+
+  allComplete: boolean = false;
+
+  updateAllComplete() {
+    this.allComplete = this.task.subtasks != null && this.task.subtasks.every(t => t.completed);
+  }
+
+  someComplete(): boolean {
+    if (this.task.subtasks == null) {
+      return false;
+    }
+    return this.task.subtasks.filter(t => t.completed).length > 0 && !this.allComplete;
+  }
+
+  setAll(completed: boolean) {
+    this.allComplete = completed;
+    if (this.task.subtasks == null) {
+      return;
+    }
+    this.task.subtasks.forEach(t => t.completed = completed);
   }
 
 }
