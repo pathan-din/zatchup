db03a076a7807b2fcb923ba8aef6de9ce48f3ed3
pathan-din <pathand@triazinesoft.com>
2020-12-10 18:51:40 +0530

    merge all

--- src/app/modules/ei/subscription/ei-subscription-update/ei-subscription-update.component.ts
+++ src/app/modules/ei/subscription/ei-subscription-update/ei-subscription-update.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseService } from 'src/app/services/base/base.service';
@@ -23,6 +24,7 @@ export class EiSubscriptionUpdateComponent implements OnInit {
 
 
   constructor(
+    private location: Location,
     private baseService: BaseService,
     private loader: NgxSpinnerService,
     private alert: NotificationService,
@@ -139,5 +141,9 @@ export class EiSubscriptionUpdateComponent implements OnInit {
       this.loader.hide();
       this.alert.error(err.error, 'Error')
     }
+  }
+
+  goBack(): void {
+    this.location.back()
   }
 }
