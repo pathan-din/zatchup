11b7acd6ba5a6ee6c85f86486b28e70bd451ea69
Rajat <rajatinvoice@gmail.com>
2020-12-28 17:23:13 +0530

    work last ei-confirmation-page new change00

--- src/app/modules/user/user-ei-confirmation/user-ei-confirmation.component.ts
+++ src/app/modules/user/user-ei-confirmation/user-ei-confirmation.component.ts
@@ -18,6 +18,7 @@ declare var $: any;
 export class UserEiConfirmationComponent implements OnInit {
   
   model: any = {};
+  editmodel: any = {};
   /*Using Validation For Manage Error Concept*/
   error: any = [];
   errorDisplay: any = {};
@@ -27,6 +28,8 @@ export class UserEiConfirmationComponent implements OnInit {
   studentsConfirmation: any = [];
   school_id:any="";
   standard_id:any;
+  classList: any[];
+  currentDate:any;
   constructor(private router: Router,
     private route: ActivatedRoute,
     private SpinnerService: NgxSpinnerService,
@@ -44,6 +47,7 @@ export class UserEiConfirmationComponent implements OnInit {
       }
     })
     this.getConfirmationDetails();
+    this.currentDate  = new Date();
   }
   
 
@@ -51,8 +55,41 @@ export class UserEiConfirmationComponent implements OnInit {
     $("#OTPModel").modal('hide');
     this.router.navigate(['user/profile-created']);
  }
+/**Delete Course  */
+deleteCourse(course_id){
 
- deleteCourse(standard_id){
+ // 
+ try {
+  let model:any={};
+  model.course_id = course_id;
+  this.SpinnerService.show()
+  this.baseService.action("user/delete-course-standard-detail-by-student/",model).subscribe(res=>{
+    let response :any ={};
+    response = res;
+    if(response.status==true){
+      this.SpinnerService.hide()
+
+      this.alert.success(response.message,"Success");
+      location.reload();
+    }else{
+      this.SpinnerService.hide();
+      var error = this.eiService.getErrorResponse(this.SpinnerService,response.error);
+      this.alert.error(error,"Error");
+     
+    }
+
+  },(error=>{
+
+    this.SpinnerService.hide()
+    this.alert.error(error.error,"Error");
+  }))
+} catch (e) {
+this.alert.error(e.error,"Error");
+}
+
+}
+/****************************** */
+ deleteStandard(standard_id){
     try {
         let model:any={};
         model.standard_id = standard_id;
@@ -92,13 +129,79 @@ export class UserEiConfirmationComponent implements OnInit {
     keyboard: false
   }); 
  }
+ openEditModel(standard_id){
+  this.editmodel={};
+  this.editmodel.standard_id = standard_id;
+ 
+  this.displayClassList(standard_id);
+  $("#personalInfoModel").modal({
+   backdrop: 'static',
+   keyboard: false
+ }); 
+}
+editStandardDetails(){
+   
+  
+  this.errorDisplay = {};
+  this.errorDisplay = this.genericFormValidationService.checkValidationFormAllControls(document.forms[1].elements, false, []);
+  if (this.errorDisplay.valid) {
+    return false;
+  }
+  try {
+
+    this.SpinnerService.show();
+    this.editmodel.standard_start_year=this.baseService.getDateFormat(this.editmodel.standard_start_year)
+    this.editmodel.standard_end_year=this.baseService.getDateFormat(this.editmodel.standard_end_year)
+    this.baseService.action("user/edit-course-standard-detail-by-student/",this.editmodel).subscribe(res=>{
+      let response:any = res;
+      if(response.status==true){
+        this.SpinnerService.hide();
+        $("#personalInfoModel").modal("hide");
+        this.alert.success("Data edit successfully","Success");
+        this.getConfirmationDetails();
+      }else{
+        this.SpinnerService.hide();
+        this.alert.error("No Update","Error");
+      }
+    },(error=>{
+      this.SpinnerService.hide();
+    }))
+  } catch (error) {
+    this.SpinnerService.hide();
+  }
+console.log("Yes");
+  
+}
+displayClassList(stId) {
+  try {
+    this.SpinnerService.show();
+    this.classList = [];
+    let data: any = {};
+    data.standard_id = stId;
+    this.baseService.getData('user/class-list-by-standardid/', data).subscribe(res => {
+      console.log(res);
+      this.SpinnerService.hide();
+      let response: any = {};
+      response = res;
+      this.classList = response.results;
+     
+    }, (error) => {
+      this.SpinnerService.hide();
+      //console.log(error);
+
+    });
+  } catch (err) {
+    this.SpinnerService.hide();
+    //console.log(err);
+  }
+}
  addPastEi(){
   $("#OTPModel").modal('hide');
   this.router.navigate(['user/add-ei'],{queryParams:{"title":"past"}});
  }
  addAnotherCourse(){
   $("#OTPModel").modal("hide");
-  this.router.navigate(['user/add-more-standard'],{queryParams:{"school_id":this.school_id }});
+   this.router.navigate(['user/add-ei'],{queryParams:{"title":"current"}});
  }
  getConfirmationDetails(){
   try{
