e4ac95867a91afa18976c5b5312ae121d1b6f21e
mukulgaur2211 <mr.mukulgaur@gmail.com>
2021-01-07 17:26:31 +0530

    merge changes of mou history

--- src/app/modules/admin/ei/admin-ei-management-document-mou-history/admin-ei-management-document-mou-history.component.ts
+++ src/app/modules/admin/ei/admin-ei-management-document-mou-history/admin-ei-management-document-mou-history.component.ts
@@ -1,33 +1,38 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { BaseService } from 'src/app/services/base/base.service';
+import { NotificationService } from 'src/app/services/notification/notification.service';
+import { DocHistory } from '../modals/education-institute.modal';
 
-export interface LastLoginUser {
+// export interface LastLoginUser {
 
-  'position': number;
-  DateOfUploading: string;
-  // profilePic: string;
-  NameofDocument: string;
-  ViewDocument: string;
-  DownloadDocument: string;
-  Remarks: string;
-  UploadedByEI: string;
-  UploadedByEmployeeName: string;
+//   'position': number;
+//   DateOfUploading: string;
+//   // profilePic: string;
+//   NameofDocument: string;
+//   ViewDocument: string;
+//   DownloadDocument: string;
+//   Remarks: string;
+//   UploadedByEI: string;
+//   UploadedByEmployeeName: string;
 
-}
+// }
 
-const ELEMENT_DATA: LastLoginUser[] = [
-  {
-    'position': 1,
-    DateOfUploading: '10 June 2020',
-    NameofDocument: 'MOU',
-    ViewDocument: 'assets/images/userWebsite/image-icon.png',
-    DownloadDocument: 'Download',
-    Remarks: '',
-    UploadedByEI: '',
-    UploadedByEmployeeName: 'Prashant(Employee 5252)',
-  }
+// const ELEMENT_DATA: LastLoginUser[] = [
+//   {
+//     'position': 1,
+//     DateOfUploading: '10 June 2020',
+//     NameofDocument: 'MOU',
+//     ViewDocument: 'assets/images/userWebsite/image-icon.png',
+//     DownloadDocument: 'Download',
+//     Remarks: '',
+//     UploadedByEI: '',
+//     UploadedByEmployeeName: 'Prashant(Employee 5252)',
+//   }
 
-];
+// ];
 
 @Component({
   selector: 'app-admin-ei-management-document-mou-history',
@@ -35,18 +40,66 @@ const ELEMENT_DATA: LastLoginUser[] = [
   styleUrls: ['./admin-ei-management-document-mou-history.component.css']
 })
 export class AdminEiManagementDocumentMouHistoryComponent implements OnInit {
+  docHistory : DocHistory;
+  EiId: any;
 
-  displayedColumns: string[] = ['position', 'DateOfUploading', 'NameofDocument', 'ViewDocument',
-    'DownloadDocument','Remarks', 'UploadedByEI', 'UploadedByEmployeeName'];
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private location: Location,
+    private baseService: BaseService,
+    private alert: NotificationService,
+    private loader: NgxSpinnerService
+    ) { 
+      this.docHistory = new DocHistory();
+    }
 
-  dataSource = ELEMENT_DATA;
-  //columnsToDisplay: string[] = this.displayedColumns.slice();
-  // dataSource: PeriodicElement[] = ELEMENT_DATA;
 
-  constructor(private router: Router) { }
-
-
-  ngOnInit() {
+  ngOnInit(): void {
+    this.docHistory.EiId = this.route.snapshot.params.ei_id;
+    this.getDocHistory();
   }
 
+  getDocHistory(page? : any){
+    this.loader.show();
+
+    this.docHistory.params ={
+      'page': page,
+      'page_size': this.docHistory.page_size,
+      'ei_id':this.docHistory.EiId,
+      'ascending_order': this.docHistory.ascendingOrder
+    }
+    this.baseService.getData('admin/ei/get_document_mou_history/', this.docHistory.params  ). subscribe(
+      (res: any) => {
+        if(res.status == true){
+          if(!page)
+          page = this.docHistory.config.currentPage
+          this.docHistory.startIndex = res.page_size * (page - 1) + 1;
+          this.docHistory.page_size = res.page_size
+          this.docHistory.config.itemsPerPage = this.docHistory.page_size
+          this.docHistory.config.currentPage = page
+          this.docHistory.config.totalItems = res.count;
+          if(res.count > 0){
+            this.docHistory.dataSource = res.results;
+            this.docHistory.pageCounts = this.baseService.getCountsOfPage()
+          }
+          else{
+            this.docHistory.dataSource = undefined;
+            this.docHistory.pageCounts = undefined; 
+          }
+        }
+        else{
+          this.loader.hide();
+          this.alert.error(res.error.message, 'Error');
+        }
+      }
+    ),
+    (err: any) => {
+      this.loader.hide();
+      this.alert.error(err, 'Error')
+    }
+  }
+  goBack(){
+    this.location.back()
+  }
 }
