706c95a8cac7289586c02d5319e5c415d0ef1f45
mukulgaur2211 <mr.mukulgaur@gmail.com>
2021-01-12 15:26:25 +0530

    merge poc changes

--- src/app/modules/admin/ei/current-poc/current-poc.component.ts
+++ src/app/modules/admin/ei/current-poc/current-poc.component.ts
@@ -1,5 +1,5 @@
 import { DatePipe, Location } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseService } from 'src/app/services/base/base.service';
@@ -30,6 +30,7 @@ import { CurrentPocDetails, PastPocDetails} from '../modals/education-institute.
 })
 
 export class CurrentPocComponent implements OnInit {
+  @ViewChild('closeModel') closeModel: ElementRef;
   pastPocDetails: PastPocDetails;
   currentPocDetails: CurrentPocDetails
   dataSource: any;
@@ -38,6 +39,7 @@ export class CurrentPocComponent implements OnInit {
   filterFromDate: any;
   filterToDate: any;
   maxDate: Date;
+
   // currentPocDetails:any;
   constructor(
     private router: Router,
@@ -72,19 +74,17 @@ export class CurrentPocComponent implements OnInit {
     this.baseService.getData('admin/get_poc_details_history_based_on_ei/', this.pastPocDetails.params).subscribe(
       (res : any) =>{
         if(res.status == true){
-          if(!page)
+          if (!page)
           page = this.pastPocDetails.config.currentPage
-          this.pastPocDetails.startIndex = res.page_size * (page - 1) + 1;
-          this.pastPocDetails.page_size = res.page_size
-          this.pastPocDetails.config.itemsPerPage = this.pastPocDetails.page_size
-          this.pastPocDetails.config.itemsPerPage = page
-          this.pastPocDetails.config.totalItems = res.count;
-          if(res.count > 0){
-            this.pastPocDetails.dataSource = res.results
-            this.pastPocDetails.pageCounts = this.baseService.getCountsOfPage()
-          }
-          else{
-            this.pastPocDetails.dataSource = undefined;
+        this.pastPocDetails.startIndex = res.page_size * (page - 1) + 1;
+        this.pastPocDetails.config.itemsPerPage = res.page_size
+        this.pastPocDetails.config.currentPage = page
+        this.pastPocDetails.config.totalItems = res.count;
+        if (res.count > 0) {
+          this.pastPocDetails.dataSource = res.results;
+        }
+        else{
+          this.pastPocDetails.dataSource = undefined
             this.pastPocDetails.pageCounts =undefined;
           }
         } else{
@@ -119,5 +119,58 @@ export class CurrentPocComponent implements OnInit {
   }
   goBack(){
     this.location.back()
+  }
+  pocDetails(data) {
+
+    
+    this.currentPocDetails.model.fullName = data.full_name;
+    // this.currentPocDetails.model.lastNAme = data.last_name;
+    this.currentPocDetails.model.phone = data.phone;
+    this.currentPocDetails.model.email = data.email;
+    this.currentPocDetails.model.ei_id = parseInt(this.ei_id);
+    this.currentPocDetails.model.employee_id = data.employee_id;
+    this.currentPocDetails.model.poc_required = true;
+    console.log('model data....',data)
+  }
+
+  changePoc() {
+    // debugger
+    // this.errorDisplay = {};
+    // this.errorDisplay = this.validationService.checkValidationFormAllControls(document.forms[0].elements, false, []);
+    // if (this.errorDisplay.valid) {
+    //   return false;
+    // }
+
+    try {
+      this.loader.show()
+      // let data = this.currentPocDetails.model
+      // data.date_of_birth = this.datePipe.transform(data.date_of_birth, 'yyyy-MM-dd')
+      this.baseService.action('admin/get_poc_details_based_on_ei/', this.currentPocDetails.model).subscribe(
+        (res: any) => {
+          if (res.status == true) {
+            this.alert.success("Added successfully", "Success");
+            this.closeModel.nativeElement.click();
+            this.getCurrentPocDetails();
+            this.getPastPocDetails();
+            //this.router.navigate(['admin/subadmin-dashboard'])
+          }
+          else {
+            if (res.error)
+              this.alert.error(res.error.message, "Error")
+            else{
+              let error =  this.baseService.getErrorResponse(this.loader, res)
+              this.alert.error(error, "Error")
+            }
+          }
+          this.loader.hide()
+        }
+      ), err => {
+        this.loader.hide()
+      }
+    }
+    catch (e) {
+      this.alert.error(e, "Error")
+      // console.log("Something Went Wrong!")
+    }
   }
 }
