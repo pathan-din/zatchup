e09b2e1eac0b8b6d075a827ad91749ddbb8928bd
pathan-din <pathand@triazinesoft.com>
2021-01-20 18:15:40 +0530

    client changes

--- src/app/modules/ei/ei.module.ts
+++ src/app/modules/ei/ei.module.ts
@@ -147,6 +147,34 @@ import { SentForApprovalComponent } from './student/sent-for-approval/sent-for-a
 import { SharedModule } from 'src/app/common/shared.module';
 import { RequestChangeDetailsComponent } from './student/request-change-details/request-change-details.component';
 import { SchooReminderComponent } from './school/schoo-reminder/schoo-reminder.component'
+import { ImageViewerModule } from 'src/app/common/image-viewer/image-viewer.module';
+
+
+const config = {
+  btnClass: 'default', // The CSS class(es) that will apply to the buttons
+  zoomFactor: 0.1, // The amount that the scale will be increased by
+  containerBackgroundColor: '#ccc', // The color to use for the background. This can provided in hex, or rgb(a).
+  wheelZoom: true, // If true, the mouse wheel can be used to zoom in
+  allowFullscreen: true, // If true, the fullscreen button will be shown, allowing the user to entr fullscreen mode
+  allowKeyboardNavigation: true, // If true, the left / right arrow keys can be used for navigation
+  btnIcons: { // The icon classes that will apply to the buttons. By default, font-awesome is used.
+    zoomIn: 'fa fa-plus',
+    zoomOut: 'fa fa-minus',
+    rotateClockwise: 'fa fa-repeat',
+    rotateCounterClockwise: 'fa fa-undo',
+    next: 'fa fa-arrow-right',
+    prev: 'fa fa-arrow-left',
+    fullscreen: 'fa fa-arrows-alt',
+  },
+  btnShow: {
+    zoomIn: true,
+    zoomOut: true,
+    rotateClockwise: true,
+    rotateCounterClockwise: true,
+    next: true,
+    prev: true
+  }
+};
 
 @NgModule({
   declarations: [
@@ -299,7 +327,8 @@ import { SchooReminderComponent } from './school/schoo-reminder/schoo-reminder.c
     AutocompleteLibModule,
     DirectiveModule,
     TooltipModule.forRoot(),
-    SharedModule
+    SharedModule,
+    ImageViewerModule.forRoot(config),
   ],
   exports: [
     EiLoginComponent
