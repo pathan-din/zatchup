3850e2c51c78d71da01fe3247ab57036556cd750
Mahesh Chand <maheshr@triazinesoft.com>
2021-03-01 15:45:32 +0530

    merge custom environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 const baseUrl='http://172.105.61.231:3000/api/';
 export const environment = {
-  production: false,
+  production: true,
   baseUrl:baseUrl,
   apiUrl:baseUrl+"user/",
   apiEiUrl:baseUrl+"ei/",
@@ -8,7 +8,6 @@ export const environment = {
   apiadminUrl:baseUrl+"admin/",
   serverImagePath:"http://staging.zatchup.com/zatchupapi/zatchup/media/temp/",
   serverVideoPath:"http://staging.zatchup.com/zatchupapi/zatchup/media/temp/",
-  // apiCommonUrl:baseUrl,
   razorPaymentApiKey:'rzp_test_i0NymYFNWzBGPK',
   debugMode:true,
   appVersion: require('../../package.json').version,
