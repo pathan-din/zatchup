de7beb79f6a27d6a235e2251a4cd99c6291cfba4
Mahesh Chand <maheshr@triazinesoft.com>
2021-03-17 19:20:48 +0530

    merge chat

--- src/app/modules/ei/ei-login/ei-login.component.ts
+++ src/app/modules/ei/ei-login/ei-login.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { GenericFormValidationService } from '../../../services/common/generic-form-validation.service';
-import { EiServiceService } from '../../../services/EI/ei-service.service';
-import { FormBuilder } from "@angular/forms";
-import { NgxSpinnerService } from "ngx-spinner"; 
+import { NgxSpinnerService } from "ngx-spinner";
 import { NotificationService } from 'src/app/services/notification/notification.service';
 import { BaseService } from 'src/app/services/base/base.service';
 import { FirebaseService } from 'src/app/services/firebase/firebase.service';
@@ -15,156 +13,110 @@ import { AngularFireAuth } from '@angular/fire/auth';
   styleUrls: ['./ei-login.component.css']
 })
 export class EiLoginComponent implements OnInit {
-  error:any=[];
-  errorDisplay:any={};
-  model:any={};
+  error: any = [];
+  errorDisplay: any = {};
+  model: any = {};
 
   constructor(
-    private genericFormValidationService:GenericFormValidationService,
     private router: Router,
-    private SpinnerService: NgxSpinnerService,
-    public eiService:EiServiceService,
-    public formBuilder: FormBuilder,
+    private loader: NgxSpinnerService,
     private alert: NotificationService,
     private baseService: BaseService,
     private firebaseService: FirebaseService,
-    private afAuth: AngularFireAuth
-    ) {}
+    private afAuth: AngularFireAuth,
+    private formValidationService: GenericFormValidationService,
+  ) { }
 
   ngOnInit(): void {
     localStorage.clear();
     sessionStorage.clear();
   }
 
-  goToEiForgetPasswordPage(){
+  goToEiForgetPasswordPage() {
     this.router.navigate(['ei/forgot-password']);
   }
 
-  goToEiContactUsPage(){
+  goToEiContactUsPage() {
     this.router.navigate(['ei/contact-us']);
   }
 
-  goToEiLoginSubadminPage(){
+  goToEiLoginSubadminPage() {
     this.router.navigate(['ei/login-subadmin']);
   }
 
-  goToEiSchoolRegisterPage(){
+  goToEiSchoolRegisterPage() {
     this.router.navigate(['ei/school-registration']);
   }
-   goToEiSubadminRegisterPage(){
+  goToEiSubadminRegisterPage() {
     this.router.navigate(['ei/subadmin-registration']);
   }
-   doLogin(){
-    this.error=[];
-    this.errorDisplay={};
-    this.errorDisplay=this.genericFormValidationService.checkValidationFormAllControls(document.forms[0].elements,false,[]);
-    if(this.errorDisplay.valid)
-    {
+  doLogin() {
+    this.error = [];
+    this.errorDisplay = {};
+    this.errorDisplay = this.formValidationService.checkValidationFormAllControls(document.forms[0].elements, false, []);
+    if (this.errorDisplay.valid) {
       return false;
     }
-	 try{
-        /**Api For the Register School */
-
-        this.SpinnerService.show(); 
-       
-        this.eiService.login(this.model).subscribe(res => {
-          console.log(res);
-          let response:any={};
-          response=res;
-          this.SpinnerService.hide(); 
-          if(response.status===true)
-          {
-
+    try {
+      this.loader.show();
+      this.baseService.action('ei/login/', this.model).subscribe(
+        (res: any) => {
+          this.loader.hide();
+          if (res.status === true) {
             this.baseService.username = this.model.username;
             this.baseService.password = this.model.password;
-            this.registerUserToFirebaseDB();
+            this.registerUserToFirebaseDB(res.data);
             this.router.navigate(['ei/otp-verification']);
-          }else{
-           
-            this.SpinnerService.hide();
-           // this.alert.error(response.error.message[0], 'Error') 
-            var errorCollection='';
-            errorCollection= this.eiService.getErrorResponse(this.SpinnerService,response.error);
-            if(errorCollection)
-              {
-                this.alert.error(errorCollection, 'Error') 
-              //alert(errorCollection);	
-              }else{
-                this.alert.error(response.message, 'Success');
-              }
-			
-            // if(response.status == false){
-            //   this.alert.error(response.error.message[0], 'Error')
-            // }
+          } else {
+            var errorCollection = '';
+            errorCollection = this.baseService.getErrorResponse(this.loader, res.error);
+            this.alert.error(errorCollection, 'Error')
           }
-         },(error) => {
-            this.SpinnerService.hide(); 
-            console.log(error);
-            this.alert.error(error, 'Error')
-            
-          });
-      }
-      catch(e){
-        console.log("Something Went Wrong!")
-      }
-    // this.router.navigate(['ei/kyc-verification']);
+        }, (error) => {
+          this.loader.hide();
+          this.alert.error(error, 'Error')
+
+        });
+    }
+    catch (e) {
+      console.log("Something Went Wrong!")
+    }
   }
-  goToEiKycVerificationPage(){
-    this.error=[];
-    this.errorDisplay={};
-    this.errorDisplay=this.genericFormValidationService.checkValidationFormAllControls(document.forms[0].elements,false,[]);
-    if(this.errorDisplay.valid)
-    {
+
+  goToEiKycVerificationPage() {
+    this.error = [];
+    this.errorDisplay = {};
+    this.errorDisplay = this.formValidationService.checkValidationFormAllControls(document.forms[0].elements, false, []);
+    if (this.errorDisplay.valid) {
       return false;
     }
-    // this.router.navigate(['ei/kyc-verification']);
   }
 
-  /************************** */
-  isValid(event)
-  {
-   if(Object.keys(this.errorDisplay).length !== 0){	  
-   this.errorDisplay=this.genericFormValidationService.checkValidationFormAllControls(document.forms[0].elements,true,[]); 
-   }
+  isValid(event) {
+    if (Object.keys(this.errorDisplay).length !== 0) {
+      this.errorDisplay = this.formValidationService.checkValidationFormAllControls(document.forms[0].elements, true, []);
+    }
   }
 
-  registerUserToFirebaseDB() {
-    // debugger
-    let email = this.isPhoneNumber(this.model.username) == true ? this.model.username + '@zatchup.com' : this.model.username
+  registerUserToFirebaseDB(data: any) {
+    let email = this.baseService.isPhoneNumber(this.model.username) == true ? this.model.username + '@zatchup.com' : this.model.username
     var that = this;
     this.afAuth.fetchSignInMethodsForEmail(email)
       .then(function (signInMethods) {
         let firebase = that.firebaseService
         if (signInMethods.length > 0) {
           console.log("yes", signInMethods);
-
         }
         else {
-          firebase.firebaseSignUp(email, email, email, that.model.password, '', "1").then(
+          firebase.firebaseSignUp(data.first_name, data.last_name, email, that.model.password, data.profile_pic, "1").then(
             (res: any) => {
               localStorage.setItem('fbtoken', res.user.uid);
-              // console.log('firebase signup res is as ::', res.user.uid)
-              // localStorage.setItem('firebaseid',res.user.uid)
-              //this.updateUserWithFirebaseID(res.user.uid)
             },
             err => {
-              // console.log('firebase signup error....', err)
+              console.log('firebase signup error....', err)
             }
           )
         }
-      })
-
-
+      })  
   }
-
-  isPhoneNumber(inputtxt) {
-    var phoneno = /^\d{10}$/;
-    if (inputtxt.match(phoneno)) {
-      return true;
-    }
-    else {
-      return false;
-    }
-  }
-  
 }
--- src/app/modules/ei/message/messages-details/messages-details.component.ts
+++ src/app/modules/ei/message/messages-details/messages-details.component.ts
@@ -1,13 +1,7 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { EiServiceService } from '../../../../services/EI/ei-service.service';
+import { Component, OnInit } from '@angular/core';
 import { BaseService } from '../../../../services/base/base.service';
-import { GenericFormValidationService } from '../../../../services/common/generic-form-validation.service';
-import { FormBuilder } from "@angular/forms";
-import { NgxSpinnerService } from "ngx-spinner";
-import { NotificationService } from 'src/app/services/notification/notification.service';
-import { Location } from '@angular/common';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { ChatService } from 'src/app/services/chat/chat.service';
 @Component({
   selector: 'app-messages-details',
   templateUrl: './messages-details.component.html',
@@ -15,43 +9,15 @@ import { AngularFirestore } from '@angular/fire/firestore';
 })
 export class MessagesDetailsComponent implements OnInit {
   model: any = {};
-  modelReason: any = {};
-  studentList: any = [];
-  studentDetails: any = [];
-  arrAge: any = [];
-  studentArr: any = [];
-  modelUserId: any = '';
-  displayedColumns: string[] = ['checked', 'SNo', 'ZatchUpID', 'Name', 'userID', 'roll_no', 'Gender', 'Age',
-    'class', 'Action'];
-  pageSize: any = 1;
-  totalNumberOfPage: any = 10;
-  config: any;
-  collection = { count: 60, data: [] };
-  dataSource: any;
-  courseList: any = [];
-  standardList: any = [];
-  classList: any = [];
-  studentListSendForBulk: any = [];
-  error: any = [];
-  errorDisplay: any = {};
-  title: any = '';
-  pageCounts: any;
-  objStudent: any = {};
   dataStudent: any = [];
   conversation: any = [];
   currentUser: any;
   recepintDetails: any = {};
+  
   constructor(
-    private router: Router,
-    private location: Location,
-    private loader: NgxSpinnerService,
-    public eiService: EiServiceService,
     public baseService: BaseService,
-    public formBuilder: FormBuilder,
-    private alert: NotificationService,
-    private route: ActivatedRoute,
-    private formValidationService: GenericFormValidationService,
-    private firestore: AngularFirestore
+    private firestore: AngularFirestore,
+    private chatService: ChatService
   ) { }
 
   ngOnInit(): void {
@@ -73,12 +39,10 @@ export class MessagesDetailsComponent implements OnInit {
 
       let data: any = {};
       let dataNew: any = {};
-      var date = new Date();
-
       data.user_friend_id = localStorage.getItem("friendlidt_id");
       data.user_send_by = localStorage.getItem('fbtoken');
       data.msg = this.model.comment;
-      data.created_on = this.baseService.getDateFormat(date);
+      data.timestamp = new Date().valueOf();
 
       this.dataStudent.push(data)
       dataNew.data = this.dataStudent;
@@ -113,8 +77,9 @@ export class MessagesDetailsComponent implements OnInit {
       }
       
     })
-    
-    
-    
+  }
+
+  getTimeAgo(time: any) {
+    return this.chatService.getTimeAgo(time)
   }
 }
