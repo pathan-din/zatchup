beb290558f7d7cb5ed1a37f6422d5f80854aa844
Mahesh Chand <maheshr@triazinesoft.com>
2021-03-22 21:30:28 +0530

    merge all

--- src/app/modules/ei/message/personal-messages/personal-messages.component.ts
+++ src/app/modules/ei/message/personal-messages/personal-messages.component.ts
@@ -14,7 +14,10 @@ export class PersonalMessagesComponent implements OnInit {
   messageData: any = [];
   currentUser: any = "";
   ids: Array<any> = [];
-
+  recepintDetails:any={};
+  conversation:any=[];
+  dataStudent:any=[];
+  objStudent:any={};
   constructor(
     private router: Router,
     private firestore: AngularFirestore,
@@ -81,10 +84,102 @@ export class PersonalMessagesComponent implements OnInit {
     })
     
   }
+  goToChatScreen(fbid) {
+    this.conversation = [];
+    this.dataStudent =[];
+    
+    this.getRecepintUserDetails(fbid)
+    return new Promise<any>((resolve, reject) => {
+      let data: any = {};
+      var date = new Date();
 
+      var uuid =fbid;
+      data.user_request_id = localStorage.getItem('fbtoken');
+      data.user_accept_id = uuid;
+      data.is_block = 0
+      data.is_seen = 0
+      data.is_active = 1
+      data.is_read = 0
+      data.created_on = this.baseService.getDateFormat(date);
+      this.getFriendListBySender(localStorage.getItem('fbtoken'), uuid, data)
+    })
+
+
+  }
+  getFriendListBySender(loginfirebase_id: any, user_accept_id: any, data) {
+    this.conversation = [];
+    this.dataStudent = [];
+    this.firestore.collection('user_friend_list').valueChanges().subscribe((res:any)=>{
+      let dataEle = res.find(elem=>{
+                      return ((elem.user_request_id===loginfirebase_id && elem.user_accept_id===user_accept_id) || (elem.user_request_id===user_accept_id && elem.user_accept_id===loginfirebase_id))  
+                    })
+           
+      if(dataEle){
+        
+        this.firestore.collection('user_friend_list').get()
+         
+        .subscribe(querySnapshot => {
+          if (querySnapshot.docs.length > 0) {
+            querySnapshot.docs.map(doc => {
+            
+              let res:any=[]
+              res=doc.data();
+             if(dataEle.user_request_id==res.user_request_id && dataEle.user_accept_id== res.user_accept_id)
+             {
+              localStorage.setItem("friendlidt_id", doc.id)
+              this. getDocumentsChat();
+              
+             }
+              
+            });
+          }
+  
+        });
+      } else{
+        this.firestore.collection("user_friend_list").add(data).then(res => {
+          localStorage.setItem("friendlidt_id",res.id)
+           this. getDocumentsChat();
+          
+         })
+      }             
+     
+      
+    })
+     
+       
+       }
+
+       
+  getDocumentsChat() {
+    this.conversation = [];
+    this.dataStudent =[];
+    var uuid= localStorage.getItem("friendlidt_id");
+    var dataSet=this.firestore.collection('chat_conversation').doc(uuid).valueChanges();
+    dataSet.subscribe((res:any)=>{
+      if(res){
+        this.conversation = res.data;
+        this.dataStudent = res.data;
+      }else{
+        this.conversation = [];
+        this.dataStudent = [];
+      }
+      this.router.navigate(["ei/messages-details"]);
+    })
+    
+    
+    
+  }
+
+  getRecepintUserDetails(uuid) {
+    localStorage.setItem("receipent",uuid);
+      this.firestore.collection('users').doc(uuid).ref.get().then(res => {
+      this.recepintDetails = res.data();
+      });
+      
+  }
   messageDetails(uid){
     localStorage.setItem('uuid', uid);
-    this.router.navigate(["user/chat"]);
+    this.router.navigate(["ei/messages-details"]);
   }
 
 }
