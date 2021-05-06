4d1a406851a395f464601e31b143df15d22aa706
radheys <radheys@triazinesoft.com>
2021-05-06 12:42:46 +0530

    data backup

--- src/app/modules/ei/reminder/reminder-list/reminder-list.component.ts
+++ src/app/modules/ei/reminder/reminder-list/reminder-list.component.ts
@@ -1,27 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import {ThemePalette} from '@angular/material/core';
+import { BaseService } from 'src/app/services/base/base.service';
+import { NgxSpinnerService } from "ngx-spinner";
+import { NotificationService } from 'src/app/services/notification/notification.service';
 
 export interface subAdminManagementElement {
-  select: string;
-  'SNo': number;
-  nameOfStudents : string;
-  class: string;
-  rollNumber: string;
+  SNo: number;
+  reminder : string;
+  time: string;
 }
 
-const ELEMENT_DATA: subAdminManagementElement[] = [
-  {select: '', 'SNo': 1, nameOfStudents: 'Wilma Mumduya', class: '', rollNumber :'53541'},
-  {select: '', 'SNo': 2, nameOfStudents: 'Wilma Mumduya', class: '', rollNumber :'53541'},
-  {select: '', 'SNo': 3, nameOfStudents: 'Wilma Mumduya', class: '', rollNumber :'53541'},
-  {select: '', 'SNo': 4, nameOfStudents: 'Wilma Mumduya', class: '', rollNumber :'53541'},
-  {select: '', 'SNo': 5, nameOfStudents: 'Wilma Mumduya', class: '', rollNumber :'53541'},
-  {select: '', 'SNo': 6, nameOfStudents: 'Wilma Mumduya', class: '', rollNumber :'53541'},
-  {select: '', 'SNo': 7, nameOfStudents: 'Wilma Mumduya', class: '', rollNumber :'53541'},
-  {select: '', 'SNo': 8, nameOfStudents: 'Wilma Mumduya', class: '', rollNumber :'53541'},
-  {select: '', 'SNo': 9, nameOfStudents: 'Wilma Mumduya', class: '', rollNumber :'53541'},
-  {select: '', 'SNo': 10, nameOfStudents: 'Wilma Mumduya', class: '', rollNumber :'53541'}
-];
+const ELEMENT_DATA: subAdminManagementElement[] = [];
 
 @Component({
   selector: 'app-reminder-list',
@@ -31,13 +21,32 @@ const ELEMENT_DATA: subAdminManagementElement[] = [
 
 export class ReminderListComponent implements OnInit {
 
-  displayedColumns: string[] = ['select', 'SNo', 'nameOfStudents', 'class', 'rollNumber'];
+  displayedColumns: string[] = ['SNo', 'message', 'recieved_date'];
 
   dataSource = ELEMENT_DATA;
   //columnsToDisplay: string[] = this.displayedColumns.slice();
   // dataSource: PeriodicElement[] = ELEMENT_DATA;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+    private baseService : BaseService,
+    private loader:NgxSpinnerService,
+    private alert:NotificationService) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getPastReminder();
+  }
+  getPastReminder(){
+    try {
+      this.baseService.getData("chat/chat_reminder_notification_present/").subscribe((res:any)=>{
+        if(res.status==true){
+          this.alert.success("Data get successfully","Success");
+          this.dataSource=res.results
+        }
+      },error=>{
+        this.loader.hide()
+      })
+    } catch (error) {
+      this.loader.hide()
+    }
+  }  
 }
